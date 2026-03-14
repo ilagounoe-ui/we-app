bda2f05fd7c7c19f349cec6fc7bc685bfd340d60
ilagounoe-ui <ilagounoe@gmail.com>
2026-03-14 16:54:11 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,18 @@
 self.addEventListener('push', function(event) {
     const options = {
-        body: 'Nouveau message sur Nexus',
-        icon: 'https://ui-avatars.com/api/?name=N&background=ff2d55',
-        badge: 'https://ui-avatars.com/api/?name=N&background=ff2d55',
+        body: 'Nouveau message sur Notre Univers !',
+        icon: 'https://ui-avatars.com/api/?name=NU&background=bb86fc&color=fff',
+        badge: 'https://ui-avatars.com/api/?name=NU&background=bb86fc&color=fff',
         vibrate: [200, 100, 200]
     };
-    event.waitUntil(self.registration.showNotification('Nexus', options));
+    event.waitUntil(
+        self.registration.showNotification('Notre Univers', options)
+    );
 });
 
 self.addEventListener('notificationclick', function(event) {
     event.notification.close();
-    event.waitUntil(clients.openWindow('./'));
+    event.waitUntil(
+        clients.openWindow('/')
+    );
 });
